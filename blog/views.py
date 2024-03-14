a6ae611729b524e9c2ea3ce5c0a167c3f232f34f
Carolina Romero Carrasco <mcarolinarc28@gmail.com>
2024-03-14 11:11:00 +0100

    Fix Internal Server Error:
    * Fixed error by correcting indentation of post_detail function in blog/templates/blog/views
    
    Signed-off-by: Carolina Romero Carrasco <mcarolinarc28@gmail.com>

--- blog/views.py
+++ blog/views.py
@@ -9,25 +9,25 @@ class PostList(generic.ListView):
     template_name = "blog/index.html"
     paginate_by = 6
 
-    def post_detail(request, slug):
-        """
-        Display an individual :model:`blog.Post`.
+def post_detail(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
 
-        **Context**
+    **Context**
 
-        ``post``
-            An instance of :model:`blog.Post`.
+    ``post``
+        An instance of :model:`blog.Post`.
 
-        **Template:**
+    **Template:**
 
-        :template:`blog/post_detail.html`
-        """
+    :template:`blog/post_detail.html`
+    """
 
-        queryset = Post.objects.filter(status=1)
-        post = get_object_or_404(queryset, slug=slug)
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
 
-        return render(
-            request,
-            "blog/post_detail.html",
-            {"post": post},
-        )
+    return render(
+        request,
+        "blog/post_detail.html",
+        {"post": post},
+    )
