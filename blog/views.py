5eccbc7eccb89265d1a9886056475d983f1bc7c1
Carolina Romero Carrasco <mcarolinarc28@gmail.com>
2024-03-15 12:38:36 +0100

    Add post comment functionality:
    * Added post method to form with user's comments to send data to server.
    * Added functionality to display messages to users once they send a comment.
    
    Signed-off-by: Carolina Romero Carrasco <mcarolinarc28@gmail.com>

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views import generic
+from django.contrib import messages
 from .models import Post
 from .forms import CommentForm
 
@@ -28,6 +29,18 @@ def post_detail(request, slug):
     post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
     comment_form = CommentForm()
 
     return render(
